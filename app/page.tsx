f819f00b4add7c9258380f89442906fc66014f11
rose-jenny <rosejenny.suelan@gmail.com>
2026-02-22 18:11:40 +0800

    Add logo and cta in landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,27 @@
-import Navigation from "@/app/components/common/navigation";
+import { Box, Button, Typography } from "@mui/material";
 import Image from "next/image";
 
 export default function Landing() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-white">
-      <main className="flex min-h-screen w-full flex-col items-center justify-between py-32 px-16 bg-white dark:bg-white sm:items-start"></main>
+      <main className="flex min-h-screen w-full flex-col items-center justify-center py-32 px-16 bg-white dark:bg-white">
+        <Image alt="tara logo" src={"/logo.png"} width={200} height={200} />
+
+        <Box className="mt-4">
+          <Typography align="center" variant="h1" color="black">
+            TARA
+          </Typography>
+          <Typography align="center" variant="body1" color="black">
+            Travel and Route Assistance
+          </Typography>
+        </Box>
+
+        <Box mt={20}>
+          <Button href="/home" variant="contained" color="success" size="large">
+            Get Started
+          </Button>
+        </Box>
+      </main>
     </div>
   );
 }
