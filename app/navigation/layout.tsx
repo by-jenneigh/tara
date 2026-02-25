de8eef49456204c45a03f59bd9819b7509ffbfb8
rose-jenny <rosejenny.suelan@gmail.com>
2026-02-25 14:25:43 +0800

    Wrap navigation with Suspense

--- app/navigation/layout.tsx
+++ app/navigation/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import Navigation from "@/app/components/common/navigation";
 import "leaflet/dist/leaflet.css";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "TARA",
@@ -14,8 +15,10 @@ export default function NavigationLayout({
 }) {
   return (
     <div className="min-h-screen flex flex-col">
-      <div className="flex-1">{children}</div>
-      <Navigation />
+      <Suspense>
+        <div className="flex-1">{children}</div>
+        <Navigation />
+      </Suspense>
     </div>
   );
 }
