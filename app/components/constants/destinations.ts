ceb88472b93b93c691232b6a6726a18f8bf43fd6
rose-jenny <rosejenny.suelan@gmail.com>
2026-02-25 13:02:10 +0800

    Update destination placeholder details

--- app/components/constants/destinations.ts
+++ app/components/constants/destinations.ts
@@ -2,39 +2,51 @@ import { DestinationType } from "@/app/components/types/destinations-type";
 
 export const DESTINATIONS = [
   {
+    id: "destination-1",
     name: "Destination #1",
-    about: "lorem ipsum dolor et amus",
+    about:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     type: DestinationType.historical,
-    images: [],
+    images: ["/placeholder_image.jpg"],
   },
   {
+    id: "destination-2",
     name: "Destination #2",
-    about: "lorem ipsum dolor et amus",
+    about:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     type: DestinationType.religious,
-    images: [],
+    images: ["/placeholder_image.jpg"],
   },
   {
+    id: "destination-3",
     name: "Destination #3",
-    about: "lorem ipsum dolor et amus",
+    about:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     type: DestinationType.recreational,
-    images: [],
+    images: ["/placeholder_image.jpg"],
   },
   {
+    id: "destination-4",
     name: "Destination #4",
-    about: "lorem ipsum dolor et amus",
+    about:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     type: DestinationType.nature,
-    images: [],
+    images: ["/placeholder_image.jpg"],
   },
   {
+    id: "destination-5",
     name: "Destination #5",
-    about: "lorem ipsum dolor et amus",
+    about:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     type: DestinationType.recreational,
-    images: [],
+    images: ["/placeholder_image.jpg"],
   },
   {
+    id: "destination-6",
     name: "Destination #6",
-    about: "lorem ipsum dolor et amus",
+    about:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     type: DestinationType.nature,
-    images: [],
+    images: ["/placeholder_image.jpg"],
   },
 ];
