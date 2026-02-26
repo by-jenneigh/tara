2c82633848106e3d6eb6daf679f2f8f5573b2e94
rose-jenny <rosejenny.suelan@gmail.com>
2026-02-26 12:02:08 +0800

    Add town in the destinations

--- app/components/constants/destinations.ts
+++ app/components/constants/destinations.ts
@@ -8,6 +8,7 @@ export const DESTINATIONS = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     type: DestinationType.historical,
     images: ["/placeholder_image.jpg"],
+    town: "Cabatuan",
     mapLocation: {
       lat: 10.949046402622345,
       lng: 122.50236868771027,
@@ -20,6 +21,7 @@ export const DESTINATIONS = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     type: DestinationType.religious,
     images: ["/placeholder_image.jpg"],
+    town: "Maasin",
     mapLocation: {
       lat: 10.879732926155558,
       lng: 122.48089313528908,
@@ -32,6 +34,7 @@ export const DESTINATIONS = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     type: DestinationType.recreational,
     images: ["/placeholder_image.jpg"],
+    town: "Janiuay",
     mapLocation: {
       lat: 10.89129595131261,
       lng: 122.43320703506471,
@@ -44,6 +47,7 @@ export const DESTINATIONS = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     type: DestinationType.nature,
     images: ["/placeholder_image.jpg"],
+    town: "Cabatuan",
     mapLocation: {
       lat: 10.949046402622345,
       lng: 122.50236868771027,
@@ -56,6 +60,7 @@ export const DESTINATIONS = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     type: DestinationType.recreational,
     images: ["/placeholder_image.jpg"],
+    town: "Maasin",
     mapLocation: {
       lat: 10.879732926155558,
       lng: 122.48089313528908,
@@ -68,6 +73,7 @@ export const DESTINATIONS = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     type: DestinationType.nature,
     images: ["/placeholder_image.jpg"],
+    town: "Janiuay",
     mapLocation: {
       lat: 10.89129595131261,
       lng: 122.43320703506471,
