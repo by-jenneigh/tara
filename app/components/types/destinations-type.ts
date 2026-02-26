63aa25bbd693ff76b1b3dde17593192e215d9c96
rose-jenny <rosejenny.suelan@gmail.com>
2026-02-26 11:52:49 +0800

    Add destination type styles map

--- app/components/types/destinations-type.ts
+++ app/components/types/destinations-type.ts
@@ -4,3 +4,15 @@ export enum DestinationType {
   historical = "Historical",
   nature = "Nature",
 }
+
+export const DESTINATION_TYPE_STYLES: Record<
+  DestinationType,
+  {
+    color: "primary" | "secondary" | "success" | "error" | "warning" | "info";
+  }
+> = {
+  [DestinationType.religious]: { color: "secondary" },
+  [DestinationType.recreational]: { color: "info" },
+  [DestinationType.historical]: { color: "warning" },
+  [DestinationType.nature]: { color: "success" },
+};
