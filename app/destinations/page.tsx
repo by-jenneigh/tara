297cb4dea82843609c94e6a4878a9210c0a0e130
rose-jenny <rosejenny.suelan@gmail.com>
2026-02-27 14:50:05 +0800

    Apply search filter result

--- app/destinations/page.tsx
+++ app/destinations/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useMemo, useState } from "react";
 import DestinationCard from "@/app/components/common/card";
 import Search from "@/app/components/common/search";
 import TopBar from "@/app/components/common/topbar";
@@ -9,32 +10,57 @@ import { useRouter } from "next/navigation";
 
 export default function Destinations() {
   const router = useRouter();
+  const [searchValue, setSearchValue] = useState("");
+
+  const filteredDestinations = useMemo(() => {
+    return DESTINATIONS.filter((d) =>
+      d.name.toLowerCase().includes(searchValue.toLowerCase()),
+    );
+  }, [searchValue]);
 
   return (
-    <Stack alignItems={"center"} className="pt-12 pb-30">
+    <Stack className="pt-12 pb-30">
       <TopBar label="Destinations" onBackClick={() => router.back()} />
-      <Stack alignContent={"center"} mb={5}>
-        <Search />
+
+      <Stack
+        sx={{
+          position: "sticky",
+          top: 0,
+          zIndex: 10,
+          bgcolor: "#d1f3c5",
+          pt: 2,
+          pb: 2,
+        }}
+      >
+        <Search
+          options={DESTINATIONS}
+          onInputChange={setSearchValue}
+          onSelect={(destination) => {
+            if (destination) {
+              router.push(`/destinations/${destination.id}`);
+            }
+          }}
+        />
       </Stack>
 
-      <Grid
-        container
-        spacing={{ xs: 2, md: 3 }}
-        columns={{ xs: 4, sm: 8, md: 12 }}
-      >
-        {DESTINATIONS.map((destination, idx) => (
-          <DestinationCard
-            key={idx}
-            name={destination.name}
-            content={destination.town}
-            image={destination.images[0]}
-            type={destination.type}
-            onCardClick={() => {
-              router.push(`/destinations/${destination.id}`);
-            }}
-          />
-        ))}
-      </Grid>
+      <Stack>
+        <Grid
+          container
+          spacing={{ xs: 2, md: 3 }}
+          columns={{ xs: 4, sm: 8, md: 12 }}
+        >
+          {filteredDestinations.map((destination) => (
+            <DestinationCard
+              key={destination.id}
+              name={destination.name}
+              content={destination.town}
+              image={destination.images[0]}
+              type={destination.type}
+              onCardClick={() => router.push(`/destinations/${destination.id}`)}
+            />
+          ))}
+        </Grid>
+      </Stack>
     </Stack>
   );
 }
