43d0eddf8e070f5c5af7b40f97812c14412167fe
rose-jenny <rosejenny.suelan@gmail.com>
2026-03-02 12:35:28 +0800

    Add origins constant

---
+++ app/components/constants/origins.ts
@@ -0,0 +1,18 @@
+export const ORIGINS = [
+  {
+    id: "airport",
+    name: "Iloilo International Airport",
+    mapLocation: {
+      lat: 10.721389,
+      lng: 122.559722,
+    },
+  },
+  {
+    id: "terminal",
+    name: "Ungka Pavia Terminal",
+    mapLocation: {
+      lat: 10.745,
+      lng: 122.5575,
+    },
+  },
+];
