7266bd5ceb69fb3fe18a8274bf09b33a9e4d7d0a
rose-jenny <rosejenny.suelan@gmail.com>
2026-03-02 12:35:43 +0800

    Fix fares layout

--- app/fares/layout.tsx
+++ app/fares/layout.tsx
@@ -12,11 +12,12 @@ export default function Layout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex items-center justify-center bg-zinc-50 font-sans dark:bg-[#d1f3c5]">
-      <div className="flex w-full h-screen flex-col items-center justify-center pt-12 pb-30 px-16 bg-[#d1f3c5] dark:bg-[#d1f3c5] sm:items-center">
+    <div className="flex items-center bg-zinc-50 font-sans dark:bg-[#d1f3c5]">
+      <main className="flex w-full min-h-screen flex-col items-center py-10 px-10 bg-[#d1f3c5] dark:bg-[#d1f3c5] sm:items-center">
         {children}
-        <Navigation />
-      </div>
+      </main>
+
+      <Navigation />
     </div>
   );
 }
