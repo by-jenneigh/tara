e4da4a000a491e23cdc452dfc7d0909cf0a6686f
rose-jenny <rosejenny.suelan@gmail.com>
2026-03-07 12:59:20 +0800

    Add hotlines constant

---
+++ app/components/constants/hotlines.ts
@@ -0,0 +1,69 @@
+export const EMERGENCY_HOTLINES = [
+  {
+    town: "Maasin",
+    hotlines: [
+      {
+        entity: "MDRRMO",
+        hotline: "09604200633",
+      },
+      {
+        entity: "Police Station",
+        hotline: "09985986216",
+      },
+      {
+        entity: "Fire Station",
+        hotline: "09638925479",
+      },
+      {
+        entity: "Health Center",
+        hotline: "09687280600",
+      },
+    ],
+  },
+  {
+    town: "Cabatuan",
+    hotlines: [
+      {
+        entity: "RTM District Hospital",
+        hotline: "(033) 5228549",
+      },
+      {
+        entity: "MDRRMO",
+        hotline: "09109403615",
+      },
+      {
+        entity: "Police Station",
+        hotline: "09985986201",
+      },
+      {
+        entity: "Fire Station",
+        hotline: "09984766711",
+      },
+      {
+        entity: "Health Center",
+        hotline: "09388388343",
+      },
+    ],
+  },
+  {
+    town: "Janiuay",
+    hotlines: [
+      {
+        entity: "MDRRMO",
+        hotline: "09309200205",
+      },
+      {
+        entity: "Police Station",
+        hotline: "09985986211",
+      },
+      {
+        entity: "Fire Station",
+        hotline: "09705509669",
+      },
+      {
+        entity: "Health Center",
+        hotline: "09755470513",
+      },
+    ],
+  },
+];
